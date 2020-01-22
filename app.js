05fe00f304d1d201aa5451af97278b72c410f5c2
Hristiyan Oysolov <47504849+Oysolov@users.noreply.github.com>
2020-01-22 15:19:17 +0200

    Initializing default table content on loading the page and resetitng the table content when loading the data from the API (#2)

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var table = document.getElementById('addressBookTable');
-var loadDataBtn = document.querySelector('.load-data-btn');
-
-
+const addressBookUrl = 'http://addressbook-api.herokuapp.com/contacts';
+const table = document.getElementById('addressBookTable');
+const loadDataBtn = document.querySelector('.load-data-btn');
 loadDataBtn.addEventListener('click', fetchAddressBookRecords);
+initAddressBookTable();
 
 function fetchAddressBookRecords() {
-    const addressBookUrl = 'http://addressbook-api.herokuapp.com/contacts';
     fetch(addressBookUrl)
         .then(response => {
             return response.json();
         })
         .then(data => {
+            resetTbody();
             var contacts = data.contacts;
             contacts.forEach(element => {
                 addRecordToAddressBook(element);
@@ -21,21 +21,29 @@ function fetchAddressBookRecords() {
         })
 }
 
-function createNode(element) {
-    return document.createElement(element);
-}
+function initAddressBookTable() {
+    var defaultProfilePicture = 'default_profile_picture.jpg';
+    let defaultRecord = {
+        avatar: defaultProfilePicture,
+        first: '',
+        last: ''
+    }
 
-function append(parent, element) {
-    return parent.appendChild(element);
+    var tbody = createTbody();
+    append(table, tbody);
+    for(var i = 0; i < 4; i++) {
+        addRecordToAddressBook(defaultRecord);
+    }
 }
 
 function addRecordToAddressBook(record) {
+    var tbody = document.querySelector('.tbody');
     let tr = createNode('tr'),
         td1 = createNode('td'),
         td2 = createNode('td'),
         td3 = createNode('td'),
         img = createNode('img');
-    
+
     img.src = record.avatar;
     img.classList.add('avatar');
     td2.textContent = record.first;
@@ -45,5 +53,25 @@ function addRecordToAddressBook(record) {
     append(tr,td1);
     append(tr,td2);
     append(tr,td3);
-    append(table, tr);
+    append(tbody, tr)
+}
+
+function createTbody() {
+    var tbody = createNode('tbody');
+    tbody.classList.add('tbody');
+    return tbody;
+}
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, element) {
+    return parent.appendChild(element);
+}
+
+function resetTbody() {
+    var oldTbody = document.querySelector('.tbody');
+    var newTbody = createTbody();
+    table.replaceChild(newTbody, oldTbody);
 }
